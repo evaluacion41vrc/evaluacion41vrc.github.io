b4b1b04a2d1596a36d7616f02bda984c06df8a21
base41vrc <base41vrc@gmail.com>
2023-03-23 22:05:43 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyAeJK06pHUmmVVZy6MKi50Bc541jubN0sg",
+    authDomain: "base41vrc1.firebaseapp.com",
+    projectId: "base41vrc1",
+    storageBucket: "base41vrc1.appspot.com",
+    messagingSenderId: "853844011075",
+    appId: "1:853844011075:web:6affacf7200dbed357ccf6",
+    measurementId: "G-11PB4ZWVLE"
+});
